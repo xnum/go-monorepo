d1cd5468c6014de25f9c6587af4ae41d776754f3
omegaatt36 <omegaatt36@gmail.com>
2021-09-20 19:50:00 +0800

    add test/tools for installing vet tools.

---
+++ tools/go.mod
@@ -0,0 +1,12 @@
+module go-monorepo/test/tools
+
+go 1.16
+
+require (
+	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/client9/misspell v0.3.4
+	github.com/gogo/protobuf v1.3.2
+	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3
+	golang.org/x/tools v0.0.0-20210106214847-113979e3529a
+	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc
+)
---
+++ tools/tools.go
@@ -0,0 +1,15 @@
+// +build tools
+
+// This file is not intended to be compiled.  Because some of these imports are
+// not actual go packages, we use a build constraint at the top of this file to
+// prevent tools from inspecting the imports.
+
+package tools
+
+import (
+	_ "github.com/client9/misspell/cmd/misspell"
+	_ "github.com/gogo/protobuf/protoc-gen-gogoslick"
+	_ "golang.org/x/lint/golint"
+	_ "golang.org/x/tools/cmd/goimports"
+	_ "honnef.co/go/tools/cmd/staticcheck"
+)
